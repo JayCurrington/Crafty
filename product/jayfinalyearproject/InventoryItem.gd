ff38dba542cd71c26f32fd2f313afc9229c421a2
Charlotte Currington <ZMAC219@live.rhul.ac.uk>
2026-01-27 16:06:27 +0000

    Added in images for inventory, working on formatting inventory and connecting to picking up items

---
+++ product/jayfinalyearproject/InventoryItem.gd
@@ -0,0 +1,12 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.texture_normal = load("res://AssetImages/InventoryItems/GrassInventory.png")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ product/jayfinalyearproject/inventory.gd
@@ -0,0 +1,12 @@
+extends GridContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#$item1.texture_normal = load("res://AssetImages/TempCatImage2.png")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
