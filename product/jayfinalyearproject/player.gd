636c18b7bb5ff8b26172ee1b1ee5bff2fc92e219
Currington Charlotte (2023) ZMAC219 <charlotte.currington.2023@live.rhul.ac.uk>
2026-01-28 22:54:21 +0000

    Revert "Restructuring the inventory to connect all them together"
    This reverts commit 2ad99d2baf6bfe510909283ed7d122bd71dae1f8

--- product/jayfinalyearproject/player.gd
+++ product/jayfinalyearproject/player.gd
@@ -82,7 +82,7 @@ func _physics_process(delta):
 #	Deal with interaction with object:
 	if Input.is_action_pressed("Interact") and nearObject != null:
 		print("Interact")
-		$Inventory/Control/TabContainer/Inventory.addToInventory(nearObject)
+		addToInventory(nearObject)
 		nearObject.isPickedUp()
 		nearObject = null
 		
