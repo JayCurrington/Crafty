9d0c50a6d9da7a4de3aba15ef01bd5086ef03163
Jay Currington <zmac219@live.rhul.ac.uk>
2026-02-16 01:45:26 +0000

    Accidentally working on main, moving to a branch

--- product/jayfinalyearproject/RecipeManager.gd
+++ product/jayfinalyearproject/RecipeManager.gd
@@ -18,7 +18,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print( recipeList[1].Name)
+	print( recipeList[0].Name)
 	pass
 	
 	
